c578c6cf7111985ca057e313215c36172a4237d5
metacoder_FEFF <metacoder.feff@gmail.com>
2026-01-15 09:55:39 +0000

    split Example.Tests.Console.csproj

---
+++ tests/Example.Tests.Console/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using AwesomeAssertions;
+global using AwesomeAssertions.Json;
+global using Newtonsoft.Json.Linq;
+
+global using Utils.Testing;
+
+global using Example.Utils;
+global using NodaTime;
