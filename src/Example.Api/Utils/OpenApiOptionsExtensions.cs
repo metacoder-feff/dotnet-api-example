3b4e111becca6ce515e5694b46b455b1cf17b15e
metacoder_FEFF <metacoder.feff@gmail.com>
2026-01-17 14:30:07 +0000

    add mapping for:OffsetDateTime, OffsetTime

--- src/Example.Api/Utils/OpenApiOptionsExtensions.cs
+++ src/Example.Api/Utils/OpenApiOptionsExtensions.cs
@@ -8,10 +8,37 @@ public static class OpenApiOptionsExtensions
 {
     public static OpenApiOptions ConfigureNodaTime(this OpenApiOptions options)
     {
-        options.MapToString<Instant>("date-time");
-        options.MapToString<LocalDate>("date");
-        //todo: continue
+        // OpenAPI specs:
+        // https://spec.openapis.org/registry/format/date-time
+        // https://spec.openapis.org/registry/format/date
+        // https://spec.openapis.org/registry/format/time
+        // based on RFC3339
+        // https://www.rfc-editor.org/rfc/rfc3339#section-5.6
 
+        // RFC "date-time"
+        // the requires allows time offset or Z
+        // ZonedDateTime: RFC does not allow ZONE
+        // LocalDateTime: RFC does not allow missing offset
+        options.MapToString<Instant>("date-time");
+        options.MapToString<OffsetDateTime>("date-time");
+
+        // OpenAPI "time"
+        // The time format represents a time as defined by full-time - RFC3339.
+        // LocalTime: RFC does not allow missing offset
+        options.MapToString<OffsetTime>("time");
+
+        // OpenAPI "date"
+        // The date format represents a date as defined by full-date - RFC3339. 
+        options.MapToString<LocalDate>("date");
+
+//TODO: other types
+        //LocalTime
+        //LocalDateTime
+        //ZonedDateTime
+        //Period
+        //Duration
+        //Interval
+        //DateInterval
         return options;
     }
 
