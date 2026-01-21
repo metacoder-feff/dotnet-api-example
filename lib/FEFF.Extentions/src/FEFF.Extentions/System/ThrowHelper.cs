55da6f7e7a5e5bb1bcebafe04ffc56bba7aabbc5
metacoder_FEFF <metacoder.feff@gmail.com>
2026-01-21 20:25:48 +0000

    ThrowHelper: add DebuggerNonUserCode

--- lib/FEFF.Extentions/src/FEFF.Extentions/System/ThrowHelper.cs
+++ lib/FEFF.Extentions/src/FEFF.Extentions/System/ThrowHelper.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace FEFF.Extentions;
 
+[DebuggerNonUserCode]
 public static class ThrowHelper
 {
     /// <summary>
