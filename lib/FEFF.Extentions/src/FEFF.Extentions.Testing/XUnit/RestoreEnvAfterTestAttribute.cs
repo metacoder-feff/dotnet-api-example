60aa0d2c239b5dd22fd3502407dc1e758a78e757
metacoder_FEFF <metacoder.feff@gmail.com>
2026-01-22 13:24:49 +0000

    fix RestoreProcessEnvironmentAfterTestAttribute

--- lib/FEFF.Extentions/src/FEFF.Extentions.Testing/XUnit/RestoreEnvAfterTestAttribute.cs
+++ lib/FEFF.Extentions/src/FEFF.Extentions.Testing/XUnit/RestoreEnvAfterTestAttribute.cs
@@ -11,18 +11,23 @@ public sealed class RestoreProcessEnvironmentAfterTestAttribute :
     BeforeAfterTestAttribute
 {
     private readonly static object __lockObj = new();
+
+    //Disallow parallel env saving or restoring Process-wide
     private static volatile Env? __oldEnv;
+    
+    // revert only allowed for the 'Case' the env was saved at.
+    private volatile IXunitTestCase? _case;
 
     public override void Before(MethodInfo methodUnderTest, IXunitTest test)
     {
         lock(__lockObj)
         {
-            //Disallow parallel run
 //TODO: correct error??
             if (__oldEnv != null)
                 throw new InvalidOperationException("Can't restore process environment in parallel tests. Consider using [Collection] attribute to all the test classes that will be part of a collection. Tests within the same collection run sequentially.");
                 
             __oldEnv = EnvironmentHelper.GetEnvironmentVariables();
+            _case = test.TestCase;
         }
     }
 
@@ -33,12 +38,16 @@ public sealed class RestoreProcessEnvironmentAfterTestAttribute :
             if(__oldEnv == null)
                 return;
 
+            if(_case != test.TestCase)
+                return;
+
             var newEnv = EnvironmentHelper.GetEnvironmentVariables();
 
             RevertOldValues(__oldEnv, newEnv);
             RemoveNewValues(__oldEnv, newEnv);
 
             __oldEnv = null;
+            _case = null;
         }
     }
 
